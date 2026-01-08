43a181fc82c97b11086ce287c0ed179ab4d9f31e
sara ashmoz <sarashmoz@gmail.com>
2026-01-08 11:55:31 +0200

    letssee

--- test/test_invoice.py
+++ test/test_invoice.py
@@ -1,12 +1,22 @@
 import unittest
 from fastapi.testclient import TestClient
 from app import app
-from db_util import init_db
+from db_util import init_db, get_db
+from db_util import save_inv_extraction
+
 
 class TestInvoiceEndpoint(unittest.TestCase):
 
     def setUp(self):
         init_db()
+
+        with get_db() as conn:
+            cur = conn.cursor()
+            cur.execute("DELETE FROM items")
+            cur.execute("DELETE FROM confidences")
+            cur.execute("DELETE FROM invoices")
+
+
         self.client = TestClient(app)
 
 
@@ -24,6 +34,29 @@ class TestInvoiceEndpoint(unittest.TestCase):
     
     
     def test_get_invoice_success(self):
+        save_inv_extraction({
+        "data": {
+            "InvoiceId": "36259",
+            "VendorName": "SuperStore",
+            "InvoiceDate": "2012-03-06T00:00:00+00:00",
+            "BillingAddressRecipient": "Aaron Bergman",
+            "ShippingAddress": "98103, Seattle, Washington, United States",
+            "SubTotal": 53.82,
+            "ShippingCost": 4.29,
+            "InvoiceTotal": 58.11,
+            "Items": [
+                {
+                    "Description": "Newell 330 Art, Office Supplies, OFF-AR-5309",
+                    "Name": "Newell 330 Art, Office Supplies, OFF-AR-5309",
+                    "Quantity": 3,
+                    "UnitPrice": 17.94,
+                    "Amount": 53.82
+                }
+            ]
+        },
+        "dataConfidence": {}
+    })
+        
         invoice_id = "36259"
 
         response = self.client.get(f"/invoice/{invoice_id}")
--- test/test_invoices_by_vendor.py
+++ test/test_invoices_by_vendor.py
@@ -1,12 +1,20 @@
 import unittest
 from fastapi.testclient import TestClient
 from app import app
-from db_util import init_db
+from db_util import init_db, get_db
+from db_util import save_inv_extraction
 
 class TestInvoicesByVendorEndpoint(unittest.TestCase):
 
     def setUp(self):
         init_db()
+
+        with get_db() as conn:
+            cur = conn.cursor()
+            cur.execute("DELETE FROM items")
+            cur.execute("DELETE FROM confidences")
+            cur.execute("DELETE FROM invoices")
+
         self.client = TestClient(app)
         
 
@@ -25,6 +33,28 @@ class TestInvoicesByVendorEndpoint(unittest.TestCase):
 
 
     def test_get_invoices_by_vendor_success(self):
+        save_inv_extraction({
+        "data": {
+            "InvoiceId": "36259",
+            "VendorName": "SuperStore",
+            "InvoiceDate": "2012-03-06T00:00:00+00:00",
+            "BillingAddressRecipient": "Aaron Bergman",
+            "ShippingAddress": "98103, Seattle, Washington, United States",
+            "SubTotal": 53.82,
+            "ShippingCost": 4.29,
+            "InvoiceTotal": 58.11,
+            "Items": [
+                {
+                    "Description": "Newell 330 Art, Office Supplies, OFF-AR-5309",
+                    "Name": "Newell 330 Art, Office Supplies, OFF-AR-5309",
+                    "Quantity": 3,
+                    "UnitPrice": 17.94,
+                    "Amount": 53.82
+                }
+            ]
+        },
+        "dataConfidence": {}
+    })
         vendor_name = "SuperStore"
 
         response = self.client.get(f"/invoices/vendor/{vendor_name}")
